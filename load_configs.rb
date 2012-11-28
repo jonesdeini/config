fd33ad3c6b1b928135845972f44dc7774d29979f
jonesdeini <jonesdeini@gmail.com>
2012-11-28 10:08:29 -0500

    add ackrc... load_configs needs fixing

--- load_configs.rb
+++ load_configs.rb
@@ -2,18 +2,19 @@ RCS = {
         "~/.pgpass" => "*:*:*:*:\"\"",
         "~/.irbrc"  => "load '~/config/.irbrc'",
         "~/.vimrc"  => "source ~/config/.vimrc",
+        "~/.ackrc"  => "source ~/config/.ackrc",
         "~/.bashrc" => "source ~/config/.bashrc"
 }
 
 def do_things
   RCS.each do |k,v|
+    # TODO if file exist, output file to user and ask to be overwritten
     write_config_to_file k,v unless File.exists?(File.expand_path(k))
   end
   Dir.mkdir(File.join(Dir.home,"bin")) unless Dir.exists?(File.join(Dir.home, "bin"))
 end
 
 def write_config_to_file(file, config)
-  # create
   f = File.new(File.expand_path(file), "w+")
   f << config
   f.chmod(0600) if file == "~/.pgpass"
